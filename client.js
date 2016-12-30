03d6b23a4b6bc61fd608682834d53fcf43186a71
Estefania Morton <estefania.morton@gmail.com>
2016-12-29 15:50:33 -1000

    can make reguests from diff urls

--- client.js
+++ client.js
@@ -1,37 +1,47 @@
 const net = require('net');
 const PORT = 8080;
 const EVENT_DATA = 'data';
-let host;
 let url;
+let body = '';
+let uri = '';
 let date = new Date();
 date.toUTCString();
-let body = '';
-let uri;
+let host;
 
 process.argv.forEach((val, index) => {
   if(index === 2) {
-    console.log(`${index}: ${val}`);
     url = val;
+
+    if(val.includes('http://')) {
+      val = val.substring(7);
+    } else if(val.includes('https://')) {
+      val = val.substring(8);
+      console.log(val)
+    }
+
     let path = val.split('/');
-    host = path[0];
-    if(path[1] === undefined) {
+    host = path.splice(0,1);
+    host = host.toString();
+    console.log(host);
+    if(path.length === 0) {
       uri = '/'
     } else {
-      uri = `/${path[1]}`;
+      path.forEach((x) => {
+        uri += `/${x}`
+      })
     }
   }
 })
 
-console.log(uri);
-let header = `GET ${uri} HTTP/1.1\r\nDate: ${date}\r\nHost: ${host}\r\nUser-Agent: estefania/curl\r\nConnection: close\r\n\r\n`
 
-if(host === 'localhost') {
-  let client = net.connect(PORT, host, () => {
+let header = `GET ${uri} HTTP/1.1\r\nDate: ${date}\r\nHost: ${host}\r\nUser-Agent: estefania/curl\r\nConnection: close\r\n\r\n`
+console.log(header);
+if(url === 'localhost') {
+  let client = net.connect(PORT, url, () => {
       client.write(header);
   })
 } else {
-
-  let client = net.createConnection(80, url, () => {
+  let client = net.createConnection(80, host, () => {
     client.write(header);
   })
 
